4bbb737b4cb618cdc7dfea520d576fc9624f0f81
aIeksancler <67743006+aIeksancler@users.noreply.github.com>
2022-05-14 04:19:39 +0200

    no console, alert windows

--- EAM Checklist Clicker.py
+++ EAM Checklist Clicker.py
@@ -7,7 +7,7 @@ from pynput import keyboard
 import cv2
 from sys import stdout
 import os
-import tkinter as tk
+#import tkinter as tk
 import ctypes
 
 
@@ -29,25 +29,23 @@ def resource_path(relative_path):
 
     return os.path.join(base_path, relative_path)
 
+
+if(auto.confirm(text='Starting. To exit press ESCAPE any time.', title='EAM Checklist Clicker', buttons=['START', 'Exit']) != 'START'):    os._exit(0)
+    
+
 ## DETERMINING SCALE
 # load targets
 print("Detecting scale...")
 try:
     target_amazonrme = Image.open(resource_path('target_amazonrme.png'))
 except:
-    print('Missing target_amazonrme.png')
-    abort = True
-
-
-if abort:
-    print('Missing target file(s). Aborting.')
-    time.sleep(5)
+    auto.alert(text='Missing target_amazonrme.png', title='EAM Checklist Clicker', button='OK')
     os._exit(0)
 
 scale = 0
 windowsScaling = ctypes.windll.shcore.GetScaleFactorForDevice(0) / 100
 
-for i in range(100, 49, -1):
+for i in range(100, 74, -1):
     w, h = target_amazonrme.size
     w = int(w*i/100)
     h = int(h*i/100)
@@ -56,7 +54,7 @@ for i in range(100, 49, -1):
         break
 
 if scale == 0:
-    for i in range(100, 151, 1):
+    for i in range(100, 126, 1):
         w, h = target_amazonrme.size
         w = int(w*i/100)
         h = int(h*i/100)
@@ -66,6 +64,9 @@ if scale == 0:
 
 if scale > 0:
     print('Closest scale found: ', i, '%')
+else:
+    auto.alert(text='Cannot detect scale. Open Checklist tab with visible Result column.', title='EAM Checklist Clicker', button='OK')
+    os._exit(0)
 
 scrollCount = int(-700 / scale / windowsScaling / (1920 / ctypes.windll.user32.GetSystemMetrics(0)))
 ## DETERMINING LANGUAGE
@@ -77,38 +78,38 @@ for language in languages:
         target_completed = Image.open(resource_path('target_completed_'+language+'.png'))
         target_completed = target_completed.resize((int(target_completed.width*scale*windowsScaling),int(target_completed.height*scale*windowsScaling)))
     except:
-        print('Missing target_completed_'+language+'.png')
-        abort = True
+        auto.alert(text='Missing target_completed'+language+'.png', title='EAM Checklist Clicker', button='OK')
+        os._exit(0)
     try:
         target_yes = Image.open(resource_path('target_yes_'+language+'.png'))
         target_yes = target_yes.resize((int(target_yes.width*scale*windowsScaling),int(target_yes.height*scale*windowsScaling)))
     except:
-        print('Missing target_yes_'+language+'.png')
-        abort = True
+        auto.alert(text='Missing target_yes'+language+'.png', title='EAM Checklist Clicker', button='OK')
+        os._exit(0)
     if auto.locateOnScreen(target_yes, confidence = 0.8, grayscale = True) != None or auto.locateOnScreen(target_completed, confidence = 0.8, grayscale = True) != None:
         print('Language detected: ', language)
         abort = False
         break
 
+
 if abort:
-    print('Language not detected. Please open Checklist tab. Aborting.')
-    time.sleep(5)
+    auto.alert(text='Cannot detect language. Make sure checklist is visible.', title='EAM Checklist Clicker', button='OK')
     os._exit(0)
     
 try:
     target_processing = Image.open(resource_path('target_processing.png'))
     target_processing = target_processing.resize((int(target_processing.width*scale*windowsScaling),int(target_processing.height*scale*windowsScaling)))
 except:
-    print('Missing target_processing.png')
-    abort = True
+    auto.alert(text='Missing target_processing.png', title='EAM Checklist Clicker', button='OK')
+    os._exit(0)
     
 try:
     target_checkbox_checked = Image.open(resource_path('target_checkbox_checked.png'))
     target_checkbox_checked = target_checkbox_checked.resize((int(target_checkbox_checked.width*scale*windowsScaling),int(target_checkbox_checked.height*scale*windowsScaling)))
     #print((int(target_checkbox_checked.width*scale*windowsScaling),int(target_checkbox_checked.height*scale*windowsScaling)))
 except:
-    print('Missing target_checkbox_checked.png')
-    abort = True
+    auto.alert(text='Missing target_checkbox_checked.png', title='EAM Checklist Clicker', button='OK')
+    os._exit(0)
   
 
 # keyboard listener
@@ -121,9 +122,9 @@ scroll = 0
 wait_after_processing = False
 
 
-window = tk.Tk()
-frame = tk.Frame()
-frame.pack()
+#window = tk.Tk()
+#frame = tk.Frame()
+#frame.pack()
 
 #window.mainloop()
 
@@ -148,6 +149,7 @@ while True:
             stdout.flush()
             i += 1
         print('')
+        time.sleep(0.3)
 
         # wait for checkboxes to appear (EAM sucks)
         for i in range(0,30):
@@ -162,7 +164,7 @@ while True:
                 print('.', end='')
                 stdout.flush()
                 time.sleep(0.1)
-            if i == 9:
+            if i == 29:
                 print('')
                 
         wait_after_processing = False
@@ -200,10 +202,10 @@ while True:
             scroll += 1
 
         # if proceeding does not prompt EAM processing, save last WO and exit program
-        if retry > 4:
+        if retry > 3:
             auto.hotkey('ctrl', 's')
             print('Done!')
-            auto.alert(text='No more checkboxes found. Closing.', title='WO KO', button='OK')
+            auto.alert(text='No more checkboxes found. Closing.', title='EAM Checklist Clicker', button='OK')
             listener.stop()
             os._exit(0)
 
