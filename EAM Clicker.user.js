98a039679bd2a2632b7746152215c1201d527d9b
aIeksancler <67743006+aIeksancler@users.noreply.github.com>
2022-06-05 20:59:41 +0200

    Update EAM Clicker.user.js

--- EAM Clicker.user.js
+++ EAM Clicker.user.js
@@ -5,7 +5,7 @@
 // @match			https://eam.eurme-amazon.com/web/base/COMMON*
 // @downloadURL		https://github.com/aIeksancler/EAM-Clicker/raw/main/EAM%20Clicker.user.js
 // @updateURL		https://github.com/aIeksancler/EAM-Clicker/raw/main/EAM%20Clicker.user.js
-// @version			4.025
+// @version			4.026
 // @grant			none
 // @run-at			document-end
 // ==/UserScript==
@@ -203,7 +203,7 @@ setInterval(function (){
     // console.log(active_tabs);
     if(active_tabs.length > 0){
         for (i = 0; i < active_tabs.legth; i++){
-            if ('Checklist' === active_tabs[i].firstChild.textContent){
+            if (active_tabs[i].firstChild.textContent.includes('Checklist')){
                 button_checkAll.style.visibility = 'visible';
                 break;
             }
